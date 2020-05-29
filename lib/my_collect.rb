b950cfa2f13a0c60f8d0777e7c14c67192cc107f
Grace Kim <goooyoon@gmail.com>
2020-05-29 21:22:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,2 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
+def collection 
 end
